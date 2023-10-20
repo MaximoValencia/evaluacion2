8de01db07e97dc9ae07f0b8c83cabd682da93d66
maximovalencia <chilab@inacap.cl>
2023-10-20 11:11:03 -0300

    vistas y url creados

--- ejercicioProductos/urls.py
+++ ejercicioProductos/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import path
 
 from productosApp.views import *
+from secondApp.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",index),
     path("juguetes/",juguetes),
     path("electronica/",electronica),
     path("ropa/",ropa),
+    path("valorant/",valorant),
+    path("league-of-legends/",lol),
+    path("age-of-empires/",aoe),
 ]
--- secondApp/views.py
+++ secondApp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request,'templatesJuegos/index.html')   
+
+def valorant(request):
+    data={
+        "titulo":"Valorant",
+        'producto1':"MAC",
+        'producto2':"Celular",
+        'producto3':"PlayStation",
+        'url':'/www.inacap.cl',
+        'imagen':'imagenes/producto.jpg'
+    }
+    return render(request,'templatesProductos/productos.html',data)
+
+def lol(request):
+    data={
+        "titulo":"League of Legends",
+        'producto1':"Pelota",
+        'producto2':"Figura de Acción",
+        'producto3':"Juego de Mesa",
+        'imagen':'imagenes/producto.jpg'
+    }
+    return render(request,'templatesProductos/productos.html',data)
+
+def aoe(request):
+    data={
+        "titulo":"Age of Empires 2",
+        'producto1':"Polera",
+        'producto2':"Chaqueta",
+        'producto3':"Zapatilla",
+        'imagen':'imagenes/producto.jpg'
+    }
+    return render(request,'templatesProductos/productos.html',data)
